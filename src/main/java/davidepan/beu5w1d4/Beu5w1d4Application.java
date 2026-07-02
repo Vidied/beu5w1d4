ceeb4a0269762af0d5a92bd78910550eca5ccb5c
Vidied <davidepan2001@gmail.com>
2026-07-02 15:23:58 +0200

    Initial commit

---
+++ src/main/java/davidepan/beu5w1d4/Beu5w1d4Application.java
@@ -0,0 +1,13 @@
+package davidepan.beu5w1d4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Beu5w1d4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Beu5w1d4Application.class, args);
+	}
+
+}
---
+++ src/test/java/davidepan/beu5w1d4/Beu5w1d4ApplicationTests.java
@@ -0,0 +1,13 @@
+package davidepan.beu5w1d4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Beu5w1d4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
